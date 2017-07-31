952f9af8f75f8b46cb24acbc096088d20c86714e
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-07-31 00:48:12 -0300

    Se agrega la clase MonticuloMinimo

---
+++ src/principalPackage/MonticuloMinimo.java
@@ -0,0 +1,30 @@
+package principalPackage;
+
+public class MonticuloMinimo extends Monticulo {
+
+	public MonticuloMinimo(int tamañoMaximo) {
+		super(tamañoMaximo);
+		this.nodo[0] = Integer.MIN_VALUE;
+	}
+
+	@Override
+	protected void montando(int i) {
+		if (!this.esHoja(i)) {
+			if (!this.condicion(this.hijoIzquierdo(i), i) || !this.condicion(this.hijoDerecho(i), i)) {
+				if (this.nodo[this.hijoIzquierdo(i)] < this.nodo[this.hijoDerecho(i)]) {
+					this.intercambiar(i, this.hijoIzquierdo(i));
+					this.montando(this.hijoIzquierdo(i));
+				} else {
+					this.intercambiar(this.hijoDerecho(i), i);
+					this.montando(this.hijoDerecho(i));
+				}
+			}
+		}
+	}
+
+	@Override
+	protected boolean condicion(int hijo, int padre) {
+		return this.nodo[hijo] >= this.nodo[padre];
+	}
+	
+}
